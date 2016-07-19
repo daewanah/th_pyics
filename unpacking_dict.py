4878a66fac537a9daa1d5eab10609c8364591418
S <s@thisisnotyourbusiness.com>
2016-07-19 11:04:54 +0800

    Revert "Unpacking dictionaries"
    This reverts commit 1f8d8a8191e91ab0c9d0e24b034696173a2fe6a4.

--- unpacking_dict.py
+++
@@ -1,8 +0,0 @@
-my_string = "Hi! My name is {name} and I live in {state}."
-my_string.format(name='Sholmergan', state='Palo Alto')
-# >>> Hi! My name is Sholmergan and I live in Palo Alto.
-my_string.format(state='Palo Alto', name='Sholmergan')
-# >>> Hi! My name is Sholmergan and I live in Palo Alto.
-my_dict = {'name': 'Sholmergan', 'state': 'Palo Alto'}
-my_string.format(**my_dict)
-# >>> Hi! My name is Sholmergan and I live in Palo Alto.
