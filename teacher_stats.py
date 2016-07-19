8e19e5d5b41a524254b3ffeb8fcf7c8c4ae967d0
S <s@thisisnotyourbusiness.com>
2016-07-19 14:15:08 +0800

    Teacher stats
    Create a function named most_classes that takes a dictionary of teachers. Each key is a teacher's name and their value is a list of classes they've taught. most_classes should return the teacher with the most classes.
    
    knowns: dict, var, len

---
+++ teacher_stats.py
@@ -0,0 +1,19 @@
+# The dictionary will be something like:
+# {'Jason Seifer': ['Ruby Foundations', 'Ruby on Rails Forms', 'Technology Foundations'],
+#  'Kenneth Love': ['Python Basics', 'Python Collections']}
+#
+# Often, it's a good idea to hold onto a max_count variable.
+# Update it when you find a teacher with more classes than
+# the current count. Better hold onto the teacher name somewhere
+# too!
+#
+# Your code goes below here.
+
+def most_classes(dict):
+    most_class = "" # holds the name of teach with most class
+    max_count = 0 # max counter for classes
+    for teacher in dict:
+        if len(dict[teacher]) > max_count:
+            max_count = len(dict[teacher])
+            most_class = teacher
+    return most_class
