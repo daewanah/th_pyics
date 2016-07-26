1fca05423802dbc3e748ad89eaeea9f1bada394e
S <s@thisisnotyourbusiness.com>
2016-07-26 16:13:48 +0800

    The Final Push
    One more push to get it all done and to play.
    
    python: 2.7.x
    comments: no
    issues: raw_input, import sys, Game()

--- character.py
+++ character.py
@@ -4,45 +4,45 @@ from combat import Combat
 
 
 class Character(Combat):
-    attack_limit = 10
-    experience = 0
-    base_hit_points = 10
+  attack_limit = 10
+  experience = 0
+  base_hit_points = 10
 
-    def attack(self):
-        roll = random.randint(1, self.attack_limit)
-        if self.weapon == 'sword':
-            roll += 1
-        elif self.weapon == 'axe':
-            roll += 2
-        return roll > 4
+  def attack(self):
+    roll = random.randint(1, self.attack_limit)
+    if self.weapon == 'sword':
+      roll += 1
+    elif self.weapon == 'axe':
+      roll += 2
+    return roll > 4
 
-    def get_weapon(self):
-        weapon_choice = raw_input("Weapon ([S]word, [A]xe, [B]ow): ").lower()
+  def get_weapon(self):
+    weapon_choice = raw_input("Weapon ([S]word, [A]xe, [B]ow): ").lower()
 
-        if weapon_choice in 'sab':
-            if weapon_choice == 's':
-                return 'sword'
-            elif weapon_choice == 'a':
-                return 'axe'
-            else:
-                return 'bow'
-        else:
-            return self.get_weapon()
+    if weapon_choice in 'sab':
+      if weapon_choice == 's':
+        return 'sword'
+      elif weapon_choice == 'a':
+        return 'axe'
+      else:
+        return 'bow'
+    else:
+      return self.get_weapon()
 
-    def __init__(self, **kwargs):
-        self.name = raw_input("Name: ")
-        self.weapon = self.get_weapon()
-        self.hit_point = self.base_hit_points
+  def __init__(self, **kwargs):
+    self.name = raw_input("Name: ")
+    self.weapon = self.get_weapon()
+    self.hit_points = self.base_hit_points
 
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    for key, value in kwargs.items():
+      setattr(self, key, value)
 
-    def __str__(self):
-        return "{}, HP: {}, XP: {}".format(self.name, self.hit_point, self.experience)
+  def __str__(self):
+    return '{}, HP: {}, XP: {}'.format(self.name, self.hit_points, self.experience)
 
-    def rest(self):
-        if self.hit_point < self.base_hit_points:
-            self.hit_point += 1
+  def rest(self):
+    if self.hit_points < self.base_hit_points:
+      self.hit_points += 1
 
-    def leveled_up(self):
-        return self.experience >= 5
+  def leveled_up(self):
+    return self.experience >= 5
--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import sys
+
 from character import Character
 from monster import Dragon
 from monster import Goblin
@@ -5,91 +7,85 @@ from monster import Troll
 
 
 class Game:
-    def setup(self):
-        self.player = Character()
-        self.monster = [
-            Goblin(),
-            Troll(),
-            Dragon()
-        ]
+  def setup(self):
+    self.player = Character()
+    self.monsters = [
+      Goblin(),
+      Troll(),
+      Dragon()
+    ]
+    self.monster = self.get_next_monster()
 
-    def get_next_monster(self):
-        try:
-            return self.monsters.pop(0)
-        except IndexError:
-            return None
+  def get_next_monster(self):
+    try:
+      return self.monsters.pop(0)
+    except IndexError:
+      return None
 
-    def monster_turn(self):
-        # Check to see if the monster attacks
-        if self.monster.attack():
-        # If so, tell the player
-            print("{} is attacking!".format(self.monster))
-        # Check if the player wants to dodge
-            if raw_input("Dodge? Y/n: ").lower() == 'y':
-        # If so, see if the dodge is successful
-                if self.player.dodge():
-        # If it is, move on
-                    print("You dodged the attack!")
-        # If it's not, remove one player hit point
-                else:
-                    print("You got hit anyway!")
-                    self.player.hit_points -= 1
-            else:
-                print("{} hit you for 1 point!".format(self.monster))
-                self.player.hit_points -= 1
-        # If the monster, isn't attacking, tell that to th player too.
+  def monster_turn(self):
+    if self.monster.attack():
+      print("{} is attacking!".format(self.monster))
+
+      if raw_input("Dodge? Y/N ").lower() == 'y':
+        if self.player.dodge():
+          print("You dodged the attack!")
         else:
-            print("{} isn't attacking this turn.".format(self.monster))
+          print("You got hit anyway!")
+          self.player.hit_points -= 1
+      else:
+        print("{} hit you for 1 point!".format(self.monster))
+        self.player.hit_points -= 1
+    else:
+      print("{} isn't attacking this turn.".format(self.monster))
 
-    def player_turn(self):
-        # Let the player attack, rest or quit
-        player_choice = raw_input("[A]ttack, [R]est, [Q]uit: ").lower()
-        # If they attack:
-        if player_choice == 'a':
-        # See if the attack is successful
-            print("You're attacking {}!".format(self.monster))
-        # If so, see if the monster dodges
-            if self.player.attack():
-        # If dodged, print that
-                if self.monster.dodge():
-                    print("{} dodged your attack!".format(self.monster))
-        # If not dodged, subtract the right num of hit point from the monster
-                else:
-                    if self.player.level_up():
-                        self.monster.hit_points -= 2
-                    else:
-                        self.monster.hit_points -= 1
-                    print("You hit {} with your {}!".format(self.monster, self.weapon))
-        # If not a good attack, tell the player
-            else:
-                print("You missed!")
-        # If they rest:
-        elif player_choice == 'r':
-        # Call the player.rest() method
-            self.player.rest()
-        # If they quit, exit the game
-        elif player_choice == 'q':
-            sys.exit()
-        # If they pick anything else, re-run this method
+  def player_turn(self):
+    player_choice = raw_input("[A]ttack, [R]est, [Q]uit? ").lower()
+    if player_choice == 'a':
+      print("You're attacking {}!".format(self.monster))
+
+      if self.player.attack():
+        if self.monster.dodge():
+          print("{} dodged your attack!".format(self.monster))
         else:
-            self.player_turn()
+          if self.player.leveled_up():
+            self.monster.hit_points -= 2
+          else:
+            self.monster.hit_points -= 1
 
-    def cleanup(self):
-        # If the monster has no more hit points:
-        # up the player's experience
-        # print a message
-        # Get a new monster
+          print("You hit {} with your {}!".format(
+              self.monster, self.player.weapon))
+      else:
+        print("You missed!")
+    elif player_choice == 'r':
+      self.player.rest()
+    elif player_choice == 'q':
+      sys.exit()
+    else:
+      self.player_turn()
 
-    def __init__(self):
-        self.setup()
+  def cleanup(self):
+    if self.monster.hit_points <= 0:
+      self.player.experience += self.monster.experience
+      print("You killed {}!".format(self.monster))
+      self.monster = self.get_next_monster()
 
-        while self.player.hit_points and (self.momster or self.monsters):
-            print(self.player)
-            self.monster_turn()
-            self.player_turn()
-            self.cleanup()
+  def __init__(self):
+    self.setup()
 
-        if self.player.hit_points:
-            print("You win!")
-        elif self.monster or self.monsters:
-            print("You lose!")
+    while self.player.hit_points and (self.monster or self.monsters):
+      print('\n'+'='*20)
+      print(self.player)
+      self.monster_turn()
+      print('-'*20)
+      self.player_turn()
+      self.cleanup()
+      print('\n'+'='*20)
+
+    if self.player.hit_points:
+      print("You win!")
+    elif self.monsters or self.monster:
+      print("You lose!")
+    sys.exit()
+
+
+Game()
