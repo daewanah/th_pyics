3c376538bb38147d34ae4c521f0bc34ed59a34ec
S <s@thisisnotyourbusiness.com>
2016-07-26 14:24:40 +0800

    Last Step for Character
    More tweaking on ’Character’ class.

--- character.py
+++ character.py
@@ -6,7 +6,7 @@ from combat import Combat
 class Character(Combat):
     attack_limit = 10
     experience = 0
-    hit_points = 10
+    base_hit_points = 10
 
     def attack(self):
         roll = random.randint(1, self.attack_limit)
@@ -32,6 +32,17 @@ class Character(Combat):
     def __init__(self, **kwargs):
         self.name = raw_input("Name: ")
         self.weapon = self.get_weapon()
+        self.hit_point = self.base_hit_points
 
         for key, value in kwargs.items():
             setattr(self, key, value)
+
+    def __str__(self):
+        return "{}, HP: {}, XP: {}".format(self.name, self.hit_point, self.experience)
+
+    def rest(self):
+        if self.hit_point < self.base_hit_points:
+            self.hit_point += 1
+
+    def leveled_up(self):
+        return self.experience >= 5
