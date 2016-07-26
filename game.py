9fc367e91329edeee94147d0f8ca563270f8fa1b
S <s@thisisnotyourbusiness.com>
2016-07-26 15:19:17 +0800

    Finishing
    Getting everything started.
    
    python: 2.7.x
    comments: yes
    issues: raw_input
    knowns: sys.exit()

--- game.py
+++ game.py
@@ -21,25 +21,58 @@ class Game:
 
     def monster_turn(self):
         # Check to see if the monster attacks
+        if self.monster.attack():
         # If so, tell the player
+            print("{} is attacking!".format(self.monster))
         # Check if the player wants to dodge
+            if raw_input("Dodge? Y/n: ").lower() == 'y':
         # If so, see if the dodge is successful
+                if self.player.dodge():
         # If it is, move on
+                    print("You dodged the attack!")
         # If it's not, remove one player hit point
+                else:
+                    print("You got hit anyway!")
+                    self.player.hit_points -= 1
+            else:
+                print("{} hit you for 1 point!".format(self.monster))
+                self.player.hit_points -= 1
         # If the monster, isn't attacking, tell that to th player too.
+        else:
+            print("{} isn't attacking this turn.".format(self.monster))
 
     def player_turn(self):
         # Let the player attack, rest or quit
+        player_choice = raw_input("[A]ttack, [R]est, [Q]uit: ").lower()
         # If they attack:
+        if player_choice == 'a':
         # See if the attack is successful
+            print("You're attacking {}!".format(self.monster))
         # If so, see if the monster dodges
+            if self.player.attack():
         # If dodged, print that
+                if self.monster.dodge():
+                    print("{} dodged your attack!".format(self.monster))
         # If not dodged, subtract the right num of hit point from the monster
+                else:
+                    if self.player.level_up():
+                        self.monster.hit_points -= 2
+                    else:
+                        self.monster.hit_points -= 1
+                    print("You hit {} with your {}!".format(self.monster, self.weapon))
         # If not a good attack, tell the player
+            else:
+                print("You missed!")
         # If they rest:
+        elif player_choice == 'r':
         # Call the player.rest() method
+            self.player.rest()
         # If they quit, exit the game
+        elif player_choice == 'q':
+            sys.exit()
         # If they pick anything else, re-run this method
+        else:
+            self.player_turn()
 
     def cleanup(self):
         # If the monster has no more hit points:
